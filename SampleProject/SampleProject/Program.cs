2b84dcc8331f014c7e16002efaa6f465ff3c1d98
primalchaos <46195034+primalchaos@users.noreply.github.com>
2024-02-04 13:55:03 +0000

    changed tabs to spaces

--- SampleProject/SampleProject/Program.cs
+++ SampleProject/SampleProject/Program.cs
@@ -8,7 +8,7 @@ namespace SampleProject
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("I just added this in Visual Studio");
-			Console.WriteLine("I just added this in the command line repo");
+            Console.WriteLine("I just added this in the command line repo");
         }
     }
 }
