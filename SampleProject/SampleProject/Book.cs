af1d7941cb5a949314f10b1854322c505c232d16
primalchaos <46195034+primalchaos@users.noreply.github.com>
2024-02-04 14:34:01 +0000

    Added Book Class

---
+++ SampleProject/SampleProject/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProject
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public List<string> Author { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+    }
+}
